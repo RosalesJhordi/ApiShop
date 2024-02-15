72fd88d73c21d3c9897f13ce42a18bc2e4e1d184
RosalesJhordi <1428060@senati.pe>
2024-02-15 18:25:32 -0500

    Creando los END-POINTS de la API

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Points de Registro y Autenticacion
+
+Route::post('Registro',[AuthController::class,'registro']);
+Route::post('Login',[AuthController::class,'login']);
+// Route::get('All',[AuthController::class,'all']);
