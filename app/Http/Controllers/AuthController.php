e21a93d9237915a6fb09a03209351100fc939cc6
RosalesJhordi <1428060@senati.pe>
2024-02-15 18:23:06 -0500

    Creando la funcion de Login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
-class Auth extends Controller
+class AuthController extends Controller
 {
     // Registro
 
@@ -30,7 +32,6 @@ class Auth extends Controller
         }
 
         //Crear usuario si pasa la validacion (Guardar los datos en la BD)
-
         $user = User::create([
             'nombres' => $request->nombres,
             'apellidos' => $request->apellidos,
@@ -40,27 +41,57 @@ class Auth extends Controller
         ]);
 
         //Obtener id del usuario creado para asignarle un token
-
         $userId = User::where('email', $request['email'])->first();
 
         //Asignar PAT token al ID
         $token = $userId->createToken('auth_token')->plainTextToken;
 
         //Retornar Token en una respuesta JSON
-
         return response()->json([
             'token' => $token
         ]);
     }
 
     //Mostrar usuarios en formato JSON
-
     public function all()
     {
-        //Selectar todos los usuarios que hay en BD
-        $users = User::all();
+        /*Selectar todos los usuarios que hay en BD -
+        Retornar respuesta JSON con los usuarios*/
+        return Response(User::all());
+    }
 
-        //Retornar respuesta JSON con los usuarios
-        return response()->json($users);
+    //Login
+    public function login(Request $request)
+    {
+
+        //Validar Datos
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        //Retornar JSON con los errores
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+
+        //Autenticar a un usuario
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json(['message' => 'No autorizado'], 401);
+        }
+
+        //Obtener id del usuario para buscar token en la BD
+        $userId = User::where('email', $request['email'])->first();
+
+
+        //Buscar y obtener Token usando el ID de usuario
+        $token = DB::table('personal_access_tokens')
+            ->where('tokenable_id', $userId->id)
+            ->pluck('token');
+
+        //Retornar Token en una respuesta JSON
+        return response()->json([
+            'token' => $token
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Auth;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',[Auth::class,'all']);
+Route::get('/',[AuthController::class,'all']);
